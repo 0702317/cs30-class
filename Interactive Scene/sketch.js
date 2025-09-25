ae83ae743274ff726bd638c1c460404b66654af5
0702317 <0702317@spsd.sk.ca>
2025-09-25 09:22:48 -0600

    added interactive scene

---
+++ Interactive Scene/sketch.js
@@ -0,0 +1,16 @@
+// Interactive Scene
+// Graham Lindsay
+// September 29th, 2025
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  circle(mouseX, mouseY, 50);
+}
