f913b9f5fa15bd76cb0264e008f03e8ae51e73ad
0702317 <0702317@spsd.sk.ca>
2025-10-15 09:52:57 -0600

    working on spacing out the particles

--- arrays-objects/sketch.js
+++ arrays-objects/sketch.js
@@ -6,16 +6,16 @@
 // - describe what you did to take this project "above and beyond"
 
 let theParticles = [];
-const particleSpacing = 100;
-const particleSize = 50;
+const particleDensity = 20;
+const particleSize = 12;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   // noStroke();
 
   // set up the particle array
-  for (let x = 0; x < width; x += particleSpacing) {
-    for (let y = 0; y < height; y += particleSpacing) {
+  for (let x = width / (particleDensity + particleSize); x < width - width / (particleDensity + particleSize); x += particleDensity) {
+    for (let y = height / (particleDensity + particleSize); y < height - height / (particleDensity + particleSize); y += particleDensity) {
       let someParticle = spawnParticle(x, y);
       theParticles.push(someParticle);
     }
@@ -24,8 +24,10 @@ function setup() {
 
 function draw() {
   background(220);
+  rect(20, 20, width - 40, height - 40);
 
   for (let someParticle of theParticles) {
+    fill(someParticle.r, someParticle.g, someParticle.b);
     circle(someParticle.xPos, someParticle.yPos, particleSize);
   }
 }
@@ -36,6 +38,11 @@ function spawnParticle(x, y) {
   particle = {
     xPos: x,
     yPos: y,
+    dx: 0,
+    dy: 0,
+    r: 255,
+    g: 255,
+    b: 255,
   };
 
   return particle;
