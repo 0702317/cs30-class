1f38552a95a0c7eb9cca15243dad1bd2fec0fca1
0702317 <0702317@spsd.sk.ca>
2025-11-03 09:31:23 -0600

    added image textures to grid character game

--- character-in-grid/sketch.js
+++ character-in-grid/sketch.js
@@ -13,6 +13,13 @@ let thePlayer = {
   x: 0,
   y: 0,
 };
+let grassImg;
+let pathImg;
+
+function preload() {
+  grassImg = loadImage("grass.png");
+  pathImg = loadImage("paving.png");
+}
 
 function setup() {
   createCanvas(windowWidth * 0.9, windowHeight * 0.9);
@@ -91,15 +98,17 @@ function displayGrid() {
   for (let y = 0; y < rows; y++) {
     for (let x = 0; x < cols; x++) {
       if (grid[y][x] === OPEN_TILE) {
-        fill(255);
+        // fill(255);
+        image(pathImg, x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE);
       }
       else if (grid[y][x] === IMPASSIBLE) {
-        fill(0);
+        // fill(0);
+        image(grassImg, x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE);
       }
       else if (grid[y][x] === PLAYER) {
         fill("red");
+        square(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE);
       }
-      square(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE);
     }
   }
 }
